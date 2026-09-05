39736c0dfadbc509d743ed6829b7abfe19d55907
HAMYL-Aththnayaka <Aththanayakayasas@gmail.com>
2026-09-06 00:24:19 +0530

    The game logic fuctions has been listed

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let playerRed ="R"
-let playerYellow = "Y"
+let playerRed = "R";
+let playerYellow = "Y";
 
 let curPlayer = playerRed;
 
@@ -7,23 +7,48 @@ let gameOver = false;
 let board;
 
 let rows = 6;
-let collumns = 7;
+let columns = 7;
 
 window.onload = function () {
     setGame();
-}
-function setGame(){
+};
+
+function setGame() {
     board = [];
-        for (let r =0 ; r < rows ; r++){
-            let row = [];
-            for(let c = 0; c< collumns ;c++){
-                row.push(" ")
+    for (let r = 0; r < rows; r++) {
+        let row = [];
+        for (let c = 0; c < columns; c++) {
+            row.push(" ");
 
-                let tile = document.createElement("div");
-                tile.id = r.toString() + "." +c.toString()
-
-                tile.classList.add("tile");
-                document.getElementById('board').append(tile)
-            }
+            let tile = document.createElement("div");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.addEventListener("click",setPiece)
+            tile.classList.add("tile");
+            document.getElementById("board").append(tile);
         }
+        board.push(row);
+    }
+}
+
+function setPiece(){
+    if(gameOver){
+        return;
+    }
+
+    let coords = this.id.split("-");
+    
+    let r = parseInt(coords[0]);
+    let c = parseInt(coords[1]);
+
+    board[r][c] =curPlayer;
+    let tile=this;
+
+    if(curPlayer ==playerRed){
+        tile.classList.add("red-piece");
+        curPlayer = playerYellow;
+    }else{
+        tile.classList.add("yellow-piece");
+        curPlayer = playerRed;
+
+    }
 }
