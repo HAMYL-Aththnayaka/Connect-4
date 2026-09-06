b9c7537240745e1793a8acc7ef85377585ef9002
HAMYL-Aththnayaka <Aththanayakayasas@gmail.com>
2026-09-06 15:07:22 +0530

    fuctionality added

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ let curPlayer = playerRed;
 let gameOver = false;
 let board;
 
+let curColumns;
+
 let rows = 6;
 let columns = 7;
 
@@ -15,6 +17,8 @@ window.onload = function () {
 
 function setGame() {
     board = [];
+    curColumns = [5 ,5 ,5 ,5 ,5 ,5 ,5];
+
     for (let r = 0; r < rows; r++) {
         let row = [];
         for (let c = 0; c < columns; c++) {
@@ -29,6 +33,84 @@ function setGame() {
         board.push(row);
     }
 }
+function checkWinner() {
+    //sliding window
+    // 1. Horizontal
+    for (let r = 0; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] !== " ") {
+                if (
+                    board[r][c] === board[r][c + 1] &&
+                    board[r][c + 1] === board[r][c + 2] &&
+                    board[r][c + 2] === board[r][c + 3]
+                ) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // 2. Vertical
+    for (let c = 0; c < columns; c++) {
+        for (let r = 0; r < rows - 3; r++) {
+            if (board[r][c] !== " ") {
+                if (
+                    board[r][c] === board[r + 1][c] &&
+                    board[r + 1][c] === board[r + 2][c] &&
+                    board[r + 2][c] === board[r + 3][c]
+                ) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // 3. Diagonal 
+    for (let r = 0; r < rows - 3; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] !== " ") {
+                if (
+                    board[r][c] === board[r + 1][c + 1] &&
+                    board[r + 1][c + 1] === board[r + 2][c + 2] &&
+                    board[r + 2][c + 2] === board[r + 3][c + 3]
+                ) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+
+    // 4. Anti-Diagonal 
+    for (let r = 3; r < rows; r++) {
+        for (let c = 0; c < columns - 3; c++) {
+            if (board[r][c] !== " ") {
+                if (
+                    board[r][c] === board[r - 1][c + 1] &&
+                    board[r - 1][c + 1] === board[r - 2][c + 2] &&
+                    board[r - 2][c + 2] === board[r - 3][c + 3]
+                ) {
+                    setWinner(r, c);
+                    return;
+                }
+            }
+        }
+    }
+}
+
+function setWinner(r ,c ){
+    let winner = document.getElementById("winner");
+    if(board[r][c] == playerRed){
+        winner.innerText = "RED Wins !!!";
+        gameOver = true;
+    }else{
+        winner.innerText = "Yellow Wins !!!";
+        gameOver = true;
+    }
+    
+}
 
 function setPiece(){
     if(gameOver){
@@ -40,8 +122,13 @@ function setPiece(){
     let r = parseInt(coords[0]);
     let c = parseInt(coords[1]);
 
+    r = curColumns[c];
+    if(r<0){
+        return;
+    }
+
     board[r][c] =curPlayer;
-    let tile=this;
+    let tile=document.getElementById( r.toString() + "-" + c.toString());
 
     if(curPlayer ==playerRed){
         tile.classList.add("red-piece");
@@ -49,6 +136,10 @@ function setPiece(){
     }else{
         tile.classList.add("yellow-piece");
         curPlayer = playerRed;
-
     }
+
+    // update the row height for the column
+    r -= 1 ;
+    curColumns[c] = r;
+    checkWinner();
 }
